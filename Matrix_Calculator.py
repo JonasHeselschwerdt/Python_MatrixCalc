4fb3b50dc53f9904cda798c3a1e0b58c6a5f6c83
KeinImperium <KeinImperium@web.de>
2025-03-07 17:05:37 +0100

    calculation in function added

--- Matrix_Calculator.py
+++ Matrix_Calculator.py
@@ -34,27 +34,27 @@ except:
 ##  User input extractor                       ##
 #################################################
 
-def getMatrix(Matrixname):  # is used to extract the matrices the user typed into the UI
+def getMatrix(matrixname):  # is used to extract the matrices the user typed into the UI
 
     with open(output_file, "r", encoding="utf-8") as user_Interface:
         lines = user_Interface.readlines()# opens the User interface for reading purposes
 
     matrix = []
 
-    if Matrixname == "A":
+    if matrixname == "A":
         matrix_lines = lines[6:9]
-    elif Matrixname == "B":
+    elif matrixname == "B":
         matrix_lines = lines[10:13]
-    elif Matrixname == "C":
+    elif matrixname == "C":
         matrix_lines = lines[14:17]
-    elif Matrixname == "Ans":
+    elif matrixname == "Ans":
         matrix_lines = lines[18:21]  # those are the line numbers of each matrix
     else:
         print("Error: No such matrix exists")
         return matrix  # return all zeros if the Matrix does not exist
 
     for line in matrix_lines:  # loops 3 times because there are 3 elements in the list matrix_lines
-        line = line.strip().replace("(", "").replace(")", "").replace(Matrixname,"").replace("=","") # Remove all Style Elements and spaces from the User Interface
+        line = line.strip().replace("(", "").replace(")", "").replace(matrixname,"").replace("=","") # Remove all Style Elements and spaces from the User Interface
     
         row = []       # Variable used to store one line each loop
    
@@ -68,3 +68,48 @@ def getMatrix(Matrixname):  # is used to extract the matrices the user typed int
         matrix.append(row)  # the row gets added to the matrix_A array
 
     return matrix
+
+
+
+
+get_calculation = ["A", "-", "B"]
+
+def calculation(get_calculation):
+    global matrix_Ans
+    matrix_1 = getMatrix(get_calculation[0])
+    matrix_2 = getMatrix(get_calculation[2])
+    operation = get_calculation[1]
+    matrix_Ans = getMatrix("Ans")
+    if operation == "*":
+        multiplication(matrix_1, matrix_2)
+        print("multiplication")
+        print(matrix_Ans)
+    if operation == "+":
+        addition(matrix_1,matrix_2)
+        print("adddition")
+        print(matrix_Ans)
+    if operation == "-":
+        subtraction(matrix_1,matrix_2)
+        print("subtraction")
+        print(matrix_Ans)
+
+def multiplication(matrix_1, matrix_2): # multiplication of two matrices
+           # creates the matrix structure of 3x3, by reading Ans matrix
+    for i in range(3):
+        for j in range(3):
+            matrix_sum = 0
+            for k in range(3):
+                matrix_sum += matrix_1[i][k] * matrix_2[k][j]
+            matrix_Ans[i][j] = matrix_sum   # storing of part results in matrix_ans
+    
+def addition(matrix_1,matrix_2):    # addition of two matrices
+    for i in range(3):
+        for j in range(3):
+            matrix_Ans[i][j] = matrix_1[i][j] + matrix_2[i][j]
+def subtraction(matrix_1,matrix_2): # subtraction of two matrices
+    for i in range(3):
+        for j in range(3):
+            matrix_Ans[i][j] = matrix_1[i][j] - matrix_2[i][j]
+
+calculation(get_calculation)
+
